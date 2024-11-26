0b0b4a63cc08526c8c4b1a3d75d9080719691e1f
Hassan <hassanabangura1@gmail.com>
2024-11-26 09:09:45 +0000

    Display Function completed

--- js/main.js
+++ js/main.js
@@ -19,7 +19,28 @@ function addBooksToLibrary(title, author, pages, isread) {
 
     // add book to library
     myLibrary.push(newBook);
+    // call diplay function
+
+    displayBook();
     
 
 }
-console.log(myLibrary)
+
+function displayBook(){
+    const bookcards = document.querySelector('.book-cards');
+    //  bookcards.innerHTML = '';
+    myLibrary.forEach((book) => {
+    const card = document.createElement('div');
+    card.className = 'card';
+    card.innerHTML = `
+    <h2>${book.title}</h2>
+    <p><strong> Author:</strong> ${book.author}</p>
+    <p><strong> Pages:</strong> ${book.pages}</p>
+    <p><strong> Read:</strong> ${book.isread ? 'yes' : 'No'}</p>
+    `;
+    bookcards.appendChild(card);
+
+})
+}
+
+addBooksToLibrary('Book One', 'John Doe', 231 , )
